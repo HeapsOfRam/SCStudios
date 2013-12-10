b00ce75469bb027ae45546189add16771ea13954
Ryan March <rmarch03@villanova.edu>
2013-12-10 17:42:05 -0500

    Our first pages

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Pages" do
+
+  describe "Home page" do
+
+  	it "should have the right title" do
+  		visit '/pages/home'
+  		expect(page).to have_title("Starcat Studios | Home")
+  	end
+
+    it "should have the content 'Starcat'" do
+      visit '/pages/home'
+      expect(page).to have_content('Starcat')
+    end
+  end
+
+  describe "Help page" do
+
+  	it "should have the right title" do
+  		visit '/pages/help'
+  		expect(page).to have_title("Starcat Studios | Help")
+  	end
+
+  	it "should have the content 'Help'" do
+  		visit '/pages/help'
+  		expect(page).to have_content('Help')
+  	end
+  end
+
+  describe "About page" do
+
+  	it "should have the right title" do
+  		visit '/pages/about'
+  		expect(page).to have_title("Starcat Studios | About")
+  	end
+  	
+  	it "should have the content 'About'" do
+  		visit '/pages/about'
+  		expect(page).to have_content('About')
+  	end
+  end
+end
