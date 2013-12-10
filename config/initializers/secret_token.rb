fc68f6526978d6f638dbc3aacb40f66e4c51f89f
Ryan March <rmarch03@villanova.edu>
2013-12-10 06:15:03 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starcats::Application.config.secret_key_base = '36f065c49eb3d575cb3ff66fab9ca6b7b779df3ab082633e65a3744502caf3bb2ae6f7c2be9b043aa49a31c65379a74fae008e93ff8962c0064d1ac96b565ddc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Starcats::Application.config.session_store :cookie_store, key: '_starcats_session'
