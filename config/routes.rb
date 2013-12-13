ece3ebe1b5c3e6eaf56cba1e6739fa4a37098e58
Ryan March <rmarch03@villanova.edu>
2013-12-13 06:01:16 -0500

    Adding some good layout and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Starcats::Application.routes.draw do
+  root 'pages#home'
+
+  match '/help', to: 'pages#help', via: 'get'
+  match '/about', to:'pages#about', via: 'get'
+  match '/contact', to: 'pages#contact', via: 'get'
+
   get "pages/home"
   get "pages/help"
   get "pages/about"
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,53 +3,49 @@ require 'spec_helper'
 describe "Pages" do
 
   describe "Home page" do
+    before { visit root_path }
 
   	it "should have the right title" do
-  		visit '/pages/home'
   		expect(page).to have_title("Starcat Studios | Home")
   	end
 
     it "should have the content 'Starcat'" do
-      visit '/pages/home'
       expect(page).to have_content('Starcat')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
   	it "should have the right title" do
-  		visit '/pages/help'
   		expect(page).to have_title("Starcat Studios | Help")
   	end
 
   	it "should have the content 'Help'" do
-  		visit '/pages/help'
   		expect(page).to have_content('Help')
   	end
   end
 
   describe "About page" do
+    before { visit about_path }
 
   	it "should have the right title" do
-  		visit '/pages/about'
   		expect(page).to have_title("Starcat Studios | About")
   	end
   	
   	it "should have the content 'About'" do
-  		visit '/pages/about'
   		expect(page).to have_content('About')
   	end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
     it "should have the right title" do
-      visit '/pages/contact'
       expect(page).to have_title("Starcat Studios | Contact")
     end
 
     it "should have the content 'Contact'" do
-      visit '/pages/contact'
       expect(page).to have_content('Contact')
     end
   end
